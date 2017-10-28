a96a095061dee33024391acf0f6e56ba5df6ceba
Ahmed Maher AlAmir <iahmedmaher@gmail.com>
2017-10-28 22:16:36 +0200

    optimizations to RR

--- process.cpp
+++ process.cpp
@@ -4,36 +4,39 @@ using namespace std;
 
 //Variables
 long remainingtime;
+long lastRun = -1;
 sigset_t sigSet;
-bool pauseIt;
 struct sigaction siga;
 
 //Functions
-//static void multi_handler(int sig, siginfo_t *siginfo, void *context) {
-//	pid_t sender_pid = siginfo->si_pid;
-//	cout << "Yo: I am " << getpid() << " and " << (int)sender_pid << " woke me up ETSARAF" << endl;
-//}
+void recordStart(int) {
+	lastRun = getClk();
+	cout << "YYYYYYYYYYYYYYYYYYYYYYYYYYYProc " << getpid() << ": lastRun now is " << lastRun << endl;
+}
+void recordEnd(int) {
+	remainingtime -= getClk() - lastRun;
+	cout << "XXXXXXXXXXXXXXXXXXXXXXXXXXProc " << getpid() << ": remTime now is " << remainingtime << endl;
+	raise(19);
+}
 
 int main(int agrc, char* argv[]) {
+	initClk();
+	lastRun = getClk();
 	cout << "Proc "<<getpid()<<": Starting with running time of " << argv[0] << endl;
 	remainingtime = strtol(argv[0], NULL, 10); //TODO: No error handling
-	pauseIt = false;
 	sigaddset(&sigSet, SIGCHLD);
 	sigaddset(&sigSet, SIGCONT);
 	sigaddset(&sigSet, SIGURG);
-	sigprocmask(SIG_UNBLOCK, &sigSet, NULL);
+	sigprocmask(SIG_UNBLOCK, &sigSet, NULL);	
 
-	/*siga.sa_sigaction = *multi_handler;
-	siga.sa_flags |= SA_SIGINFO;
+	signal(SIGCONT, recordStart);
+	signal(SIGTSTP, recordEnd);
 
-	sigaction(SIGCONT, &siga, NULL);*/
-
-	while (remainingtime > 0) {
-		//TODO: Linux is trash and so am I
+	while (remainingtime > (getClk() - lastRun)) {
 		sleep(1);
 		cout << "Proc " << getpid() << ": TICK TICK" << endl;
-		remainingtime--;
 	}
 	cout << "Proc " << getpid() << ": finished! Returning ..." << endl;
+	destroyClk(false);
 	return 0;
 }
